b657597c39f6b466aabeda65d90cfd17fc07befc
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-06 15:40:22 +0000

    q, struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Write, stdout, Read, Result, Error};
 use ratatui::termion::raw::IntoRawMode;
+use ratatui::termion::event::{Key, Event};
+use ratatui::termion::input::TermRead;
 
 struct Editor {
 }
@@ -8,12 +10,24 @@ impl Editor {
     fn default() -> Self {Editor{}}
     fn run(&self) -> Result<(), > {
         let mut stdout = stdout().into_raw_mode()?;
+        write!(stdout, "{}{}q to exit", termion::clear::All, termion::cursor::Goto(1,1)).unwrap();
+        stdout.flush().unwrap();
+        /*
         for b in io::stdin().bytes() {
             let c = b.unwrap() as char;
             println!("{}", c);
             if c == 'q' {
                 break;
             }
+        }
+        */
+        for c in stdin.events() {
+            let evt = c.unwrap();
+            match evt {
+                Event::Key(Key::char('q')) => break;
+            }
+            _ => {}
+            stdout.flush().unwrap();
         }
         Ok(())
     }
