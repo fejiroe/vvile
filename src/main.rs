59efc65f29aaad9ef4626f024f4bb9bce581d294
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-06 19:22:43 +0000

    printing row

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use std::io::{self, Write, stdin, stdout, Read, Result, Error};
+use std::io::{stdin, stdout, Result, Error, Read, Write};
 use ratatui::termion::raw::IntoRawMode;
-use ratatui::termion::event::{Key, Event};
 use ratatui::termion::input::TermRead;
+use ratatui::termion::event::Key;
+use ratatui::termion::terminal_size;
+use std::env;
 
 struct Editor {
 }
 
 impl Editor {
     fn default() -> Self {Editor{}}
-    fn run(&self) -> Result<(), > {
+    fn run(&self) -> Result<(),> {
+        // let mut size = terminal_size().unwrap();
         let stdin = stdin();
         let mut stdout = stdout().into_raw_mode()?;
         write!(stdout,
-            "{}{}ctrl q to exit",
+            "{}{}",
             ratatui::termion::clear::All,
             ratatui::termion::cursor::Goto(1,1)
             ).unwrap();
@@ -20,6 +23,12 @@ impl Editor {
         for k in stdin.keys() {
             let key = k.unwrap();
             match key {
+                Key::Char('a') => Self::draw_rows(&self)?,
+                // Key::Char('i') => ,
+                // Key::Char('x') => ,
+                // Key::Char('s') => ,
+                // Key::Char('r') => ,
+                // Key::Char('v') => ,
                 Key::Ctrl(c) => match c {
                     'q' => break, 
                     _ => {}
@@ -30,9 +39,26 @@ impl Editor {
         }
         Ok(())
     }
+    fn draw_rows(&self) -> Result<(),> {
+        let height = terminal_size()?.1;
+        for current_row in 0..height {
+            print!("~");
+            if current_row + 1 < height {
+                print!("\r\n")
+            }
+        }
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), > {
     Editor::default().run();
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_something() {}
 }
