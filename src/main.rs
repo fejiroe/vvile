5bf6ae60b2b07621d8757dec8e4050f98135acbc
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-06 15:25:07 +0000

    q, struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::io::{self, Write, stdout, Read, Result, Error};
 use ratatui::termion::raw::IntoRawMode;
 
-fn main() -> Result<(),> {
-    let mut stdout = stdout().into_raw_mode()?;
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+struct Editor {
+}
+
+impl Editor {
+    fn default() -> Self {Editor{}}
+    fn run(&self) -> Result<(), > {
+        let mut stdout = stdout().into_raw_mode()?;
+        for b in io::stdin().bytes() {
+            let c = b.unwrap() as char;
+            println!("{}", c);
+            if c == 'q' {
+                break;
+            }
+        }
+        Ok(())
     }
+}
+
+fn main() -> Result<(), > {
+    let editor = Editor::default();
+    editor.run();
     Ok(())
 }
